1db52903c1ba4441fd990e3f5695bd85e8b381ea
Wang <151574942+zw23a@users.noreply.github.com>
2024-05-29 16:57:18 -0400

    Update robot_test.py
    solve the problem about there are only two mode of bump hit. 
    Now, three mode of bump hit can change the state of the robot. Then the speed of the motor need to be modified to escape from the wall and not let the robot stack on the wall

--- robot_test.py
+++ robot_test.py
@@ -1,154 +1,88 @@
 from pololu_3pi_2040_robot import robot
-
 from pololu_3pi_2040_robot.extras import editions
-
 import time
 
- 
-
 display = robot.Display()
-
 bump_sensors = robot.BumpSensors()
-
 motors = robot.Motors()
-
-bump_sensors = robot.BumpSensors()
-
 buzzer = robot.Buzzer()
-
-display = robot.Display()
-
 yellow_led = robot.YellowLED()
-
-encoders = robot.Encoders()
-
- 
 
 edition = editions.select()
 
 if edition == "Standard":
-
     max_speed = 1500
-
     turn_time = 250
-
 elif edition == "Turtle":
-
     max_speed = 3000
-
     turn_time = 500
-
 elif edition == "Hyper":
-
     max_speed = 1125
-
     turn_time = 150
-
     motors.flip_left(True)
-
     motors.flip_right(True)
 
-    
-
 display.fill(0)
-
 display.show()
-
 bump_sensors.calibrate()
-
- 
-
-time.sleep_ms(1000)
+time.sleep(1)
 
 while True:
-
     motors.set_speeds(max_speed, max_speed)
-
     bump_sensors.read()
 
- 
+    left_pressed = bump_sensors.left_is_pressed()
+    right_pressed = bump_sensors.right_is_pressed()
 
-    if bump_sensors.left_is_pressed():
-
+    if left_pressed and right_pressed:
+        # Both bumpers are pressed
         yellow_led.on()
-
         motors.set_speeds(0, 0)
-
-        buzzer.play("a32")
-
         display.fill(0)
-
+        display.text("Both", 0, 0)
+        display.show()
+        buzzer.play("g32")
+        # Perform specific action for both bumpers pressed
+        motors.set_speeds(-max_speed / 2, -max_speed / 2)
+        time.sleep(turn_time / 1000)
+        motors.set_speeds(max_speed / 2, -max_speed / 2)
+        time.sleep(turn_time / 1000)
+        motors.set_speeds(0, 0)
+        yellow_led.off()
+        display.fill(0)
+        display.show()
+    elif left_pressed:
+        # Left bumper is pressed
+        yellow_led.on()
+        motors.set_speeds(0, 0)
+        display.fill(0)
         display.text("Left", 0, 0)
-
         display.show()
-
-        motors.set_speeds(max_speed/2, -max_speed/2)
-
-        time.sleep_ms(turn_time)
-
+        buzzer.play("c32")
+        # Perform specific action for left bumper pressed
+        motors.set_speeds(-max_speed/5, -max_speed/5)
+        time.sleep(turn_time / 1000)
+        motors.set_speeds(max_speed/3, -max_speed/3)
+        time.sleep(turn_time / 1000)
         motors.set_speeds(0, 0)
-
-        buzzer.play("b32")
-
         yellow_led.off()
-
         display.fill(0)
+        display.show()        
 
-        display.show()
-
-        if bump_sensors.right_is_pressed():
-
-            yellow_led.on()
-
-            motors.set_speeds(0, 0)
-
-            buzzer.play("c32")
-
-            display.fill(0)
-
-            display.text("Both", 44, 0)
-
-            display.show()
-            
-            motors.set_speeds(-max_speed/2, -max_speed/2)
-
-            motors.set_speeds(0, 0)
-
-
-
- 
-
-    if bump_sensors.right_is_pressed():
-
+    elif right_pressed:
+        # Right bumper is pressed
         yellow_led.on()
-
         motors.set_speeds(0, 0)
-
-        buzzer.play("e32")
-
         display.fill(0)
-
         display.text("Right", 88, 0)
-
         display.show()
-
- 
-
-        motors.set_speeds(-max_speed/2, max_speed/2)
-
-        time.sleep_ms(turn_time)
-
- 
-
+        buzzer.play("e32")
+        # Perform specific action for right bumper pressed
+        motors.set_speeds(-max_speed/5, -max_speed/5)
+        time.sleep(turn_time / 1000)
+        motors.set_speeds(-max_speed/3, max_speed/3)
+        time.sleep(turn_time / 1000)
         motors.set_speeds(0, 0)
-
-        buzzer.play("f32")
-
         yellow_led.off()
-
- 
-
         display.fill(0)
-
-        display.show()
-
+        display.show()   

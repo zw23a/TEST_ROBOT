9a9dd37bd7564f3a32096421e96c55c2e5ba0462
Wang <151574942+zw23a@users.noreply.github.com>
2024-05-29 15:11:10 -0400

    Update robot_test.py
    5.29 edit

--- robot_test.py
+++ robot_test.py
@@ -12,7 +12,11 @@ bump_sensors = robot.BumpSensors()
 
 motors = robot.Motors()
 
+bump_sensors = robot.BumpSensors()
+
 buzzer = robot.Buzzer()
+
+display = robot.Display()
 
 yellow_led = robot.YellowLED()
 
@@ -26,25 +30,25 @@ if edition == "Standard":
 
     max_speed = 1500
 
-    turn_time = 0.25  # Converted to seconds
+    turn_time = 250
 
 elif edition == "Turtle":
 
     max_speed = 3000
 
-    turn_time = 0.5  # Converted to seconds
+    turn_time = 500
 
 elif edition == "Hyper":
 
     max_speed = 1125
 
-    turn_time = 0.15  # Converted to seconds
+    turn_time = 150
 
     motors.flip_left(True)
 
     motors.flip_right(True)
 
-   
+    
 
 display.fill(0)
 
@@ -54,25 +58,33 @@ bump_sensors.calibrate()
 
  
 
-time.sleep(1000)  # Converted to seconds
+time.sleep_ms(1000)
 
 while True:
-    motors.set_speeds(max_speed, max_speed)
-    bump_sensors.read()
-     if bump_sensors.left_is_pressed():
-     	if bump_sensors.left_is_pressed():
-     	   yellow_led.on()
-           motors.set_speeds(0, 0)
-           buzzer.play("a32")
-        display.fill(0)
-        display.text("Left", 0, 0)
-        display.show()
-        
-        motors.set_speeds(max_speed // 2, -max_speed // 2)
 
-        time.sleep(turn_time)
+    motors.set_speeds(max_speed, max_speed)
+
+    bump_sensors.read()
 
  
+
+    if bump_sensors.left_is_pressed():
+
+        yellow_led.on()
+
+        motors.set_speeds(0, 0)
+
+        buzzer.play("a32")
+
+        display.fill(0)
+
+        display.text("Left", 0, 0)
+
+        display.show()
+
+        motors.set_speeds(max_speed/2, -max_speed/2)
+
+        time.sleep_ms(turn_time)
 
         motors.set_speeds(0, 0)
 
@@ -80,17 +92,33 @@ while True:
 
         yellow_led.off()
 
- 
-
         display.fill(0)
 
         display.show()
 
+        if bump_sensors.right_is_pressed():
+
+            yellow_led.on()
+
+            motors.set_speeds(0, 0)
+
+            buzzer.play("c32")
+
+            display.fill(0)
+
+            display.text("Both", 44, 0)
+
+            display.show()
+            
+            motors.set_speeds(-max_speed/2, -max_speed/2)
+
+            motors.set_speeds(0, 0)
+
+
+
  
 
     if bump_sensors.right_is_pressed():
-
-        single_bump_triggered = True
 
         yellow_led.on()
 
@@ -106,51 +134,9 @@ while True:
 
  
 
-        motors.set_speeds(-max_speed // 2, max_speed // 2)
+        motors.set_speeds(-max_speed/2, max_speed/2)
 
-        time.sleep(turn_time)
-
- 
-
-        motors.set_speeds(0, 0)
-
-        buzzer.play("f32")
-
-        yellow_led.off()
-
- 
-
-        display.fill(0)
-
-        display.show()
-
- 
-
-    if not single_bump_triggered and bump_sensors.right_is_pressed() and bump_sensors.left_is_pressed():
-
-        yellow_led.on()
-
-        motors.set_speeds(0, 0)
-
-        buzzer.play("e32")
-
-        display.fill(0)
-
-        display.text("Both", 88, 0)
-
-        display.show()
-
- 
-
-        motors.set_speeds(-max_speed // 2, -max_speed // 2)
-
-        time.sleep(turn_time)
-
-       
-
-        motors.set_speeds(max_speed // 3, -max_speed // 3)
-
-        time.sleep(turn_time)
+        time.sleep_ms(turn_time)
 
  
 
@@ -160,6 +146,9 @@ while True:
 
         yellow_led.off()
 
+ 
+
         display.fill(0)
 
         display.show()
+

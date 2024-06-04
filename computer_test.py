3e80b87c48316de0bd0808225dedf5863088022f
Wang <151574942+zw23a@users.noreply.github.com>
2024-06-04 17:51:58 -0400

    Update computer_test.py

--- computer_test.py
+++ computer_test.py
@@ -4,7 +4,7 @@ import threading
 import time
 
 # Configure the serial port
-ser = serial.Serial('COM1', 57600)  # Replace 'COM1' with the correct port for your Wixel module
+ser = serial.Serial('COM4', 9600)  # Replace 'COM1' with the correct port for your Wixel module
 # ser = serial.Serial('/dev/ttyACM0', 9600)
 
 # Function to send data

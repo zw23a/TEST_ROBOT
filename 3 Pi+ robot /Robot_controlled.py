6a0e4537c340ceb720b201edc0ba9cfb4a53d0d7
Wang <151574942+zw23a@users.noreply.github.com>
2024-07-11 16:24:00 -0400

    Update Robot_controlled.py

--- 3 Pi+ robot /Robot_controlled.py
+++ 3 Pi+ robot /Robot_controlled.py
@@ -1,13 +1,14 @@
-import time
-from machine import Pin, UART
 from pololu_3pi_2040_robot import robot
 from pololu_3pi_2040_robot.extras import editions
+from machine import UART, Pin
+import time
 
-bump_sensors = robot.BumpSensors()
-motors = robot.Motors()
 display = robot.Display()
-edition = editions.select()
+motors = robot.Motors()
 yellow_led = robot.YellowLED()
+
+edition = editions.select()
+
 if edition == "Standard":
     max_speed = 1500
     turn_time = 250
@@ -21,26 +22,45 @@ elif edition == "Hyper":
     motors.flip_right(True)
 
 
-#  UART，RX  5，TX  4 (adjust by real)
-uart = UART(0, baudrate=9600, tx=Pin(28), rx=Pin(29))
-yellow_led.on()
-display.fill(0)
-display.text("Ready", 80, 0)
-display.show()
+def initialize_uart():
+    # Initialize UART with appropriate TX and RX pins
+    uart = UART(0, baudrate=9600, tx=Pin(28), rx=Pin(29))  # Adjust TX and RX pins as necessary
+    return uart
 
-try:
-    while True:
-        if uart.any():
-            command = uart.read(1).decode()
-            if command == 'F':
-                motors.set_speeds(max_speed,max_speed)
-            elif command == 'B':
-                motors.set_speeds(-max_speed,-max_speed)
-            elif command == 'L':
-                motors.set_speeds(0, max_speed/2)
-            elif command == 'R':
-                motors.set_speeds(max_speed/2, 0)
-            elif command == 'S':
-                motors.set_speeds(0, 0)  
-except KeyboardInterrupt:
-    print("Exiting...")
+def read_command(uart):
+    if uart.any():
+        command = uart.read(100)  # Adjust the number of bytes as needed
+        return command
+    return None
+
+uart = initialize_uart()
+display.fill(0)
+display.show()
+time.sleep(1)
+cmd1 = b'x01' #stright
+cmd2 = b'x00' #right
+
+while True:
+    command  = read_command(uart)
+    if command == cmd1:
+
+        #straight
+        yellow_led.on()
+        display.fill(0)
+        display.text("Straight", 0, 0)
+        display.show()
+        motors.set_speeds(max_speed,max_speed)
+        
+        
+    if command ==cmd2:
+
+        #stop and turn
+        yellow_led.off()
+        motors.set_speeds(0, 0)
+        display.fill(0)
+        display.text("Right", 0, 0)
+        display.show()
+        motors.set_speeds(1000,-1000)
+        
+
+    

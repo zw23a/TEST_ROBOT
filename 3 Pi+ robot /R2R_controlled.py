cd8b2ab0e8479be5c4aef7aab478da10ddb0701d
Wang <151574942+zw23a@users.noreply.github.com>
2024-07-11 16:02:51 -0400

    Create R2R_controlled.py

---
+++ 3 Pi+ robot /R2R_controlled.py
@@ -0,0 +1,66 @@
+from pololu_3pi_2040_robot import robot
+from pololu_3pi_2040_robot.extras import editions
+from machine import UART, Pin
+import time
+
+display = robot.Display()
+motors = robot.Motors()
+yellow_led = robot.YellowLED()
+
+edition = editions.select()
+
+if edition == "Standard":
+    max_speed = 1500
+    turn_time = 250
+elif edition == "Turtle":
+    max_speed = 3000
+    turn_time = 500
+elif edition == "Hyper":
+    max_speed = 1125
+    turn_time = 150
+    motors.flip_left(True)
+    motors.flip_right(True)
+
+
+def initialize_uart():
+    # Initialize UART with appropriate TX and RX pins
+    uart = UART(1, baudrate=9600, tx=Pin(28), rx=Pin(29))  # Adjust TX and RX pins as necessary
+    return uart
+
+def read_command(uart):
+    if uart.any():
+        command = uart.read(100)  # Adjust the number of bytes as needed
+        return command
+    return None
+
+uart = initialize_uart()
+display.fill(0)
+display.show()
+time.sleep(1)
+cmd1 = b'x01' #stright
+cmd2 = b'x00' #right
+
+while True:
+    command  = read_command(uart)
+    if command == cmd1:
+
+        #straight
+        yellow_led.on()
+        display.fill(0)
+        display.text("Straight", 0, 0)
+        display.show()
+        motors.set_speeds(max_speed,max_speed)
+        time.sleep(turn_time/200)
+        
+    if command ==cmd2:
+
+        #stop and turn
+        yellow_led.off()
+        motors.set_speeds(0, 0)
+        display.fill(0)
+        display.text("Right", 0, 0)
+        display.show()
+        motors.set_speeds(1000,-1000)
+        time.sleep(turn_time/125)
+
+    
